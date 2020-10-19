ebc8d03871948206e0dbeb50411601c6a62d4e40
Akiff Manji <akiff.manji@gmail.com>
2020-10-19 16:45:12 -0700

    Updates user service create method
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- chat-server/src/services/users/users.class.js
+++ chat-server/src/services/users/users.class.js
@@ -1,5 +1,20 @@
 const { Service } = require('feathers-nedb');
 
 exports.Users = class Users extends Service {
-  
+    create(data, params) {
+        const { firstName, lastName, address, city, province, postalCode, email, password } = data;
+        const userData = {
+            firstName,
+            lastName,
+            address,
+            city,
+            province,
+            postalCode,
+            // These are just a temporary holdover so local authentication doesnt break
+            email,
+            password
+        };
+
+        return super.create(userData, params);
+    }
 };
--- chat-server/src/services/users/users.service.js
+++ chat-server/src/services/users/users.service.js
@@ -6,7 +6,7 @@ const hooks = require('./users.hooks');
 module.exports = function (app) {
   const options = {
     Model: createModel(app),
-    paginate: app.get('paginate')
+    // paginate: app.get('paginate')
   };
 
   // Initialize our service with any options it requires
