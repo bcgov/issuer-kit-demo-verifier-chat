2e21dfa13282d32bd6f7bdf6604fa54668e8f188
Akiff Manji <akiff.manji@gmail.com>
2020-10-20 12:10:58 -0700

    Updated disclaimer component with i18n content
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- chat-client/src/app/app.component.ts
+++ chat-client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { TranslateService } from '@ngx-translate/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chat-client';
+
+  constructor(translate: TranslateService) {
+    translate.setDefaultLang('en');
+    translate.use('en');
+  }
 }
--- chat-client/src/app/shared/shared.module.ts
+++ chat-client/src/app/shared/shared.module.ts
@@ -4,11 +4,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule
-  ],
   exports: [
     CommonModule,
     FormsModule,
